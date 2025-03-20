1cbea6aec43efd8a0747372a6df1086ba8a86d1f
bettoraite <bettoraite@gmail.com>
2025-03-20 10:07:41 +0500

    feat: add support transform

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import './seed';
+import * as cookieParser from 'cookie-parser';
+import corsConfig from './config/corsConfig';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  app.enableCors(corsConfig);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap();
+void bootstrap();
